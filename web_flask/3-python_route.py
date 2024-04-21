844736f3ee061f0db91cb28d8dbd9c5382b98111
EstherKiguru <esipraisewambui@gmail.com>
2024-04-21 16:09:47 +0000

    web flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays "HBNB" """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Displays 'C' followed by the value of text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """displays "Python", followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    """Starts the web development server"""
+    app.run(host='0.0.0.0', port=5000)
