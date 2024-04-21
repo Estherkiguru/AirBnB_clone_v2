5c6fe0a93a0b5f2b0671b9d3ed16604edf6e5c27
EstherKiguru <esipraisewambui@gmail.com>
2024-04-21 08:53:14 -0700

    web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- script that starts a Flask web application
+script that starts a Flask web application
 """
 from flask import Flask
 app = Flask(__name__)
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""Displays 'Hello HBNB!"""
-	return "Hello HBNB!"
+    """Displays 'Hello HBNB!"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
@@ -17,6 +17,7 @@ def hbnb():
     """displays "HBNB" """
     return 'HBNB'
 
+
 if __name__ == "__main__":
-	"""Starts the web development server"""
-	app.run(host='0.0.0.0', port=5000)
+    """Starts the web development server"""
+    app.run(host='0.0.0.0', port=5000)
